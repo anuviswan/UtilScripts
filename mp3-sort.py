280c4e86c02b1a30a68c2fb86d2590a6e5426697
Anu Viswan <anu.viswan@gmail.com>
2018-12-31 14:49:24 +0530

    Move files to Sub-Folders

--- mp3-sort.py
+++ mp3-sort.py
@@ -2,7 +2,10 @@
 from eyed3 import id3
 import os
 import sys
+import shutil
+import re
 
+# read command line arg : Folder Name
 arg = sys.argv[1]
 
 # Get Album/Artist Name for given mp3
@@ -11,22 +14,33 @@ def GetAlbumName(filename):
 	tag.parse(filename)
 	albumName = tag._getAlbum()
 	artistName = tag._getArtist()
-	
-	if(albumName=="None"):
-		if(artistName=="None"):
+	print(albumName)
+	if(albumName==None):
+		if(artistName==None):
 			return "Unsorted"
 		else:
 			return artistName
 	else:
 		return albumName
 
+# Return all files in the folder that has the specified extension
 def GetAllFilesFromFolder(folderPath,extension):
 	return (os.path.join(folderPath,file) for file in os.listdir(folderPath) if file.endswith('.' + extension))
 
+# Sort files into Album/Artist Names
 def Sort_Mp3(folderPath):
 	files = GetAllFilesFromFolder(folderPath,"mp3")
 	for file in files:
-		print(GetAlbumName(file))
+		albumName = GetAlbumName(file)
+		
+		filename = "Unsorted"
+		if re.match(r'[\\/:"*?<>|]+', albumName):
+			filename = os.path.basename(file)
+			print(filename)
+		albumFolder = os.path.join(folderPath, filename)
+		if not os.path.exists(albumFolder):
+			os.makedirs(albumFolder)
+		shutil.move(file,os.path.join(albumFolder,filename))
 	
 
 Sort_Mp3(arg)
